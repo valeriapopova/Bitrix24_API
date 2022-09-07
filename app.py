b33a8479b6846a53778c4d0565e024a19317744a
valeriapopova <iamvaleriapopova@yandex.ru>
2022-09-07 14:57:18 +0400

    post lead

---
+++ app.py
@@ -0,0 +1,32 @@
+from bitrix24 import Bitrix24
+from flask import Flask, request, Response
+from werkzeug.exceptions import BadRequestKeyError
+
+from config import Configuration
+
+app = Flask(__name__)
+app.config.from_object(Configuration)
+
+
+@app.route('/bitrix/post', methods=['POST', 'GET'])
+def post_bitrix():
+    if request.method == 'POST':
+        try:
+            json_file = request.get_json(force=False)
+            name = json_file['data'][0]['name']
+            phone = json_file['data'][1]['phone']
+            url = json_file['url']
+            bx24 = Bitrix24(url)
+            r = bx24.callMethod("crm.lead.add",
+                                fields={
+                                    "NAME": name,
+                                    "PHONE": [{"VALUE": phone, "VALUE_TYPE": "WORK"}],
+                                    "ASSIGNED_BY_ID": 1,
+                                },
+                                params={"REGISTER_SONET_EVENT": "Y"})
+            return Response("Создан лид", 201)
+
+        except BadRequestKeyError:
+            return Response("Пустое значение", 400)
+
+
